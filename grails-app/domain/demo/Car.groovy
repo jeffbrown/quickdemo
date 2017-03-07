36ae9705585d9aef49442a118281f3322b6c592d
Jeff Scott Brown <brownj@ociweb.com>
2017-03-07 06:41:35 -0700

    sample

---
+++ grails-app/domain/demo/Car.groovy
@@ -0,0 +1,9 @@
+package demo
+
+import grails.rest.Resource
+
+@Resource(uri='/automobiles')
+class Car {
+    String make
+    String model
+}
---
+++ grails-app/init/quickdemo/BootStrap.groovy
@@ -0,0 +1,9 @@
+package quickdemo
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
